316c53dbfda8e8e6f61dbf2b020703bea369f1ee
code <codecode60.protonmail.com>
2022-04-12 10:08:53 +0930

    Time Limit functional

--- arithmetic.js
+++ arithmetic.js
@@ -1,50 +1,79 @@
 'use strict'
 
 // Session Timer
-let inSession = false;
+document.getElementById('timeLimit').value ? document.getElementById('time').innerText = `Time: ${document.getElementById('timeLimit').value}` :
+document.getElementById('time').innerText = `Time: 0`
+let inSession = 1;
 let sessSecs = 0;
-let timerInterval 
+let timeLimitSeconds = document.getElementById('timeLimit').value
+let timerInterval
+
 document.getElementById("answer").addEventListener("keypress", e => {
-    if (inSession===false) {
-        inSession = true;
+    if (inSession===1) {
+        inSession = 0;
         timerInterval = setInterval(timer, 989)
         document.getElementById("pulse").style.setProperty('display','block')
 } } ); 
 
 const timer = () => {
     // console.log(performance.now())
-    document.getElementById('time').innerText = `Time: ${++sessSecs}`;
+        console.log(timeLimitSeconds)
+    if (timeLimitSeconds) {
+        if (timeLimitSeconds > 1) {
+        document.getElementById('time').innerText = `Time: ${--timeLimitSeconds}`;
+    }
+    else {
+            document.getElementById('time').innerText = `Time: ${--timeLimitSeconds}`;
+            clearInterval(timerInterval);
+            document.getElementById("pulse").style.setProperty('display','none');
+            inSession = -1;
+        }
+    }
+    else {
+    document.getElementById('time').innerText = `Time: ${++sessSecs}`; 
+    }
 }
 
 function sessReset () {
     clearInterval(timerInterval)
-    inSession = false;
+    inSession = 1;
     sessSecs = 0;
     sessScore = 0;
+    timeLimitSeconds = document.getElementById('timeLimit').value;
+    console.log(timeLimitSeconds)
     
+    if (timeLimitSeconds) {
+        document.getElementById('time').innerText = `Time: ${timeLimitSeconds}`;
+    }
+    else {
+        document.getElementById('time').innerText = `Time: ${sessSecs}`;
+    }
+
     document.getElementById("pulse").style.setProperty('display','none')    
     document.getElementById('score').innerText = `Correct: ${sessScore}`;
-    document.getElementById('time').innerText = `Time: ${sessSecs}`;
 }
 
 // Question Generator
 let leftNumber; 
 let rightNumber;
 let opChosen;
+let sessScore = 0
 
-let leftMin = document.querySelector("#sMLtop > input").value;
-let leftMax = document.querySelector("#sMLbot > input").value;
-let rightMin = document.querySelector("#sMRtop > input").value;
-let rightMax = document.querySelector("#sMRbot > input").value;
 
 
 const qGen = () => {
+    //Parameter check
+    let leftMin = document.querySelector("#sMLtop > input").value;
+    let leftMax = document.querySelector("#sMLbot > input").value;
+    let rightMin = document.querySelector("#sMRtop > input").value;
+    let rightMax = document.querySelector("#sMRbot > input").value;
+    
     //Number generation
-    // console.log(leftMin,leftMax)
     leftNumber = parseInt(Math.random()*(1+Math.abs(leftMax-leftMin)) + Number(leftMin));
     rightNumber = parseInt(Math.random()*(1+Math.abs(rightMax-rightMin)) + Number(rightMin));
     document.getElementById('leftNo').innerText = `${leftNumber}`;
     document.getElementById('rightNo').innerText = `${rightNumber}`;
+    // console.log("qGen",leftNumber,document.getElementById('leftNo').innerText,leftMin)
     
     //Operand check
     let opSpec = document.querySelector("#sMMinput > input").value;
@@ -59,7 +88,7 @@ const qGen = () => {
     
     //Operand generation
     opChosen = opSelected[parseInt(Math.random()*opSelected.length)]
-    console.log(opChosen,opSelected,opSelected.length)
+    // console.log(opChosen,opSelected,opSelected.length)
     opChosen ? document.getElementById('operand').innerText = opChosen : opChosen = '+';
 
     //Division, Subtraction fix
@@ -76,9 +105,9 @@ const qGen = () => {
                 leftNumber = temp;
                 document.getElementById('leftNo').innerText = `${leftNumber}`;
                 document.getElementById('rightNo').innerText = `${rightNumber}`;
-            }
         }
     }
+}
     
     // Build a decPrecision variable setting!! Done: false;
     // let decPrecision = ElementID.value
@@ -86,11 +115,13 @@ const qGen = () => {
 
 const qCheck = () => {
     let stringQ = `${leftNumber}` + opChosen + `${rightNumber}`;
-    console.log(stringQ)
+    // console.log(stringQ)
 
     if (document.getElementById('answer').value === `${(eval(stringQ)).toFixed(0)}`) {
         document.getElementById('answer').value = ''
+        if (inSession === 0) {
         document.getElementById('score').innerText = `Correct: ${++sessScore}`;
+        }
         qGen();
     }
 }
@@ -101,7 +132,6 @@ document.getElementById('answer').addEventListener('keyup', e => {
 qGen()
 
 // Session Score
-let sessScore = 0
 
 
 // Settings Button
@@ -121,16 +151,25 @@ sButton.addEventListener("click", e => {
 
 sClose.addEventListener("click", e => {
     closeNGen()
+    sessReset()
     sWrapper.style.setProperty("display", "none")
 } );
 document.addEventListener("keypress", e => {
     if (window.getComputedStyle(sWrapper).display === 'block') {
         if (e.shiftKey && e.code === 'Enter') {
-            console.log("that tickles!")
+            // console.log("that tickles!")
                 // closeNGen()
             document.getElementById("settingsClose").focus();
             }
-    } } );
+    }
+    if (window.getComputedStyle(sWrapper).display === 'grid') {
+        if (e.shiftKey && e.code === 'Enter') {
+            // console.log("that tickles!")
+                // closeNGen()
+            document.getElementById("settingsButton").focus();
+            }
+    }
+} );
 
 function closeNGen() {
     sWrapper.style.setProperty("z-index", "998");
